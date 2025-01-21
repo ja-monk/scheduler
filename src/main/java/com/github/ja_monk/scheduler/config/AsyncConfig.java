d6207fd9ee8d2ab6cee45bc961cb893085ddb41c
ja-monk <188385736+ja-monk@users.noreply.github.com>
2025-01-21 23:31:45 +0000

    add async config

---
+++ src/main/java/com/github/ja_monk/scheduler/config/AsyncConfig.java
@@ -0,0 +1,8 @@
+package com.github.ja_monk.scheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {}
